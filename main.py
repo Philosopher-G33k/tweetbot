bb054240227d22bb5b5fca6931449e1eaf582780
Ishan Malviya <ishan.malviya@gmail.com>
2021-02-07 02:51:51 +0530

    Exports dependencies to requirements.txt, integrates hackernews API

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
 import os
+from hackernews import hn
 
 def main():
+    news_client = hn.NewsClient()
     print("The system info is given below:")
     print(sys.platform)
     print(os.cpu_count())
+    print(str(news_client.get_best_story(fetchMax=10)[0]))
+
 
 
 if __name__ == '__main__':
