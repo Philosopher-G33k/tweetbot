6897a749384ceff5fb33089d24857387ce225ce6
Ishan Malviya <ishan.malviya@gmail.com>
2021-02-24 02:40:45 +0530

    Checks for auth status before posting tweet, if auth expired, reauthenticates

--- twittermanager.py
+++ twittermanager.py
@@ -10,11 +10,26 @@ class TwitterManager:
         self.api = tweepy.API(auth)
 
 
+    def reauthenticate(self):
+        load_dotenv()
+        auth = tweepy.OAuthHandler(os.getenv("TWITTER_CONSUMER_KEY"), os.getenv("TWITTER_CONSUMER_SECRET"))
+        auth.set_access_token(os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
+        self.api = tweepy.API(auth)
+
+
     def tweet(self, content):
         print("=============")
         print(content)
         print("tweet method called")
-        self.api.update_status(content)
+        # test authentication
+        try:
+            self.api.verify_credentials()
+            print("Authentication OK")
+        except:
+            print("Error during authentication")
+            self.reauthenticate()
+        finally:
+            self.api.update_status(content)
 
 
 
